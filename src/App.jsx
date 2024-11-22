fe34e6f1b07f6df0d5834752bd208adf6cd02757
believer297 <amaraymen5@gmail.com>
2024-11-22 14:03:13 +0100

    initialised project + added sidebar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Page from "./app/dashboard/page";
+import { ThemeProvider } from "./components/theme-provider";
+
+function App() {
+  return (
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Page />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/app-sidebar.jsx
@@ -0,0 +1,35 @@
+//import * as React from "react";
+
+import data from "@/lib/data";
+import { NavMain } from "@/components/nav-main";
+import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+export function AppSidebar({ ...props }) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      {/* ---SideBar Header--- */}
+      <SidebarHeader>
+        <TeamSwitcher schools={data.schools} />
+      </SidebarHeader>
+
+      {/* ---SideBar Content--- */}
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+
+      {/* ---SideBar Footer--- */}
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
